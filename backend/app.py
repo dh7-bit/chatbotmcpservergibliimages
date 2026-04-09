608db841852df7ea66335803cc8ee0f637ab97d8
Saksham Gupta <sakshamgupta0155@gmail.com>
2026-04-09 23:23:52 +0530

    s

--- backend/app.py
+++ backend/app.py
@@ -129,5 +129,8 @@ def generate_image_route():
 # -------------------------------
 # RUN APP
 # -------------------------------
+
+
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
